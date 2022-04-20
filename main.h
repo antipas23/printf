1ca0d5fcdf60f0365539584d5caaace6ef91c4b9
antipas23 <antipasgetu123@gmail.com>
2022-04-20 23:19:11 +0300

    edited

--- main.h
+++ main.h
@@ -4,15 +4,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _putchar(char p);
-int print_n(const va_list ls);
+int _putchar(char x);
 int _printf(const char *format, ...);
 int print_char(const va_list a);
 int print_string(const va_list b);
 int print_modulo(const va_list list);
-int num_digits(int n);
-void print_num(int n);
-int print_b(const va_list);
-int len(const char *s);
-
 #endif
