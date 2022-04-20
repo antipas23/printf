624bc787c2625cb24421cedbc8fe1559237b66f7
antipas23 <antipasgetu123@gmail.com>
2022-04-20 22:11:14 +0300

    This is a creating a printf with partner

---
+++ _putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _putchar - this is a given char
+ * @x: tis is the char
+ * Return: 1
+**/
+
+
+int _putchar(char x)
+{
+	char y = x;
+
+	write(1, &y, 1);
+
+	return (1);
+}
