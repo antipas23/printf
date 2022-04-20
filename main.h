bc5ea17d6077136163b1ad4e1cca8498bce6403e
antipas23 <antipasgetu123@gmail.com>
2022-04-20 23:10:45 +0300

    this is mainh

--- main.h
+++ main.h
@@ -1,12 +1,18 @@
 #ifndef ZAL
 #define ZAL
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
-
-int _putchar(char x);
+int _putchar(char p);
+int print_n(const va_list ls);
 int _printf(const char *format, ...);
+int print_char(const va_list a);
+int print_string(const va_list b);
+int print_modulo(const va_list list);
+int num_digits(int n);
+void print_num(int n);
+int print_b(const va_list);
+int len(const char *s);
 
-
+#endif
