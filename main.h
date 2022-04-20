d2b5fe9c0590197873b4046829493e3dbaed8b4e
antipas23 <antipasgetu123@gmail.com>
2022-04-20 23:28:41 +0300

    edited

--- main.h
+++ main.h
@@ -5,8 +5,13 @@
 #include <unistd.h>
 
 int _putchar(char x);
+int print_n(const va_list ls);
 int _printf(const char *format, ...);
 int print_char(const va_list a);
 int print_string(const va_list b);
 int print_modulo(const va_list list);
+int num_digits(int n);
+void print_num(int n);
+int print_b(const va_list);
+int len(const char *s);
 #endif
